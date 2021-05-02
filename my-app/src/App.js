c471c98a944f7e947d883681f2b284c03fd67d42
Pavel Sharovatov <pavel.js.html@gmail.com>
2021-05-02 15:39:09 +0300

    Html test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,9 +5,25 @@ import eda from './eda.js';
 function App() {
   return (
     <div className="App">
+      <div className="header">
+        <div className="wrapper">
+          <div className="logo">
+            <a href="#"><img src={logo}/></a>
+          </div>
+          <nav className="nav">
+            <ul>
+              <li><a href="#">Professional</a></li>
+              <li><a href="#">Professional</a></li>
+              <li><a href="#">Professional</a></li>
+              <li><a href="#">Professional</a></li>
+              <li><a href="#">Professional</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
       <button type="button" className="mainButton">Найти еду</button>
       <div className="eda">
-        
+
       </div>
     </div>
   );
