a3ad52bd83851816ba4e967a2cabafbf394bd6e0
TedNorman <pavvlad-97@mail.ru>
2021-05-02 14:56:50 +0300

    start app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import eda from './eda.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button type="button" className="mainButton">Найти еду</button>
+      <div className="eda">
+        
+      </div>
     </div>
   );
 }
